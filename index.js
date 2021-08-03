9577b9706a8ac05820029d091a9082357ea24442
eric <wenxiangdong@outlook.com>
2021-08-04 00:10:55 +0800

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+const postcss = require('postcss')
+const blackSelectorList = {
+  weapp:['\\\\?:focus-within','\\\\?:hover','\\\\?:focus','>',"\\*"],
+  tt:['\\\\?:focus-within','\\\\?:hover','\\\\?:focus'],
+}
+
+const index = process.argv.findIndex(item => item === '--type')
+const type = index === -1 ?  'weapp' : process.argv[index + 1]
+
+function isSupported(selector,blackList) {
+  return !blackList.some(item => new RegExp(item).test(selector))
+}
+
+
+module.exports = postcss.plugin('postcss-taro-tailwind', (options = {}) => {
+  return root => {
+    root.walkRules(rule => {
+      if (rule.parent.name === 'media') {
+        rule.parent.remove()
+      }
+      if (!isSupported(rule.selector,blackSelectorList[type])) {
+        rule.remove()
+      }
+
+      rule.walkDecls(decl => {
+        if (decl.prop === 'visibility') {
+          switch (decl.value) {
+            case 'hidden':
+              decl.replaceWith(decl.clone({ value: 'collapse'}))
+              return
+          }
+        }
+
+        if (decl.prop === 'vertical-align') {
+          switch (decl.value) {
+            case 'middle':
+              decl.replaceWith(decl.clone({ value: 'center'}))
+              return
+          }
+        }
+
+        // allow using rem values (default unit in tailwind)
+        if (decl.value.includes('rem') && !decl.value.includes('--')) {
+          options.debug && console.log('replacing rem value', decl.prop, decl.value, '=>', '' + (parseFloat(decl.value) * 16))
+          decl.value = '' + (parseFloat(decl.value) * 16) + 'px'
+        }
+      })
+    })
+  }
+})
+
